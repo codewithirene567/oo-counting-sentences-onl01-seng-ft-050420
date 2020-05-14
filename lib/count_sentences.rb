e867372f8f422a071fa7d6622414bf252d56eedd
Irene Scott <irenejinscott@gmail.com>
2020-05-14 17:32:12 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,25 @@
 require 'pry'
 
 class String
-
+  
+attr_accessor :sentence
+  
   def sentence?
-    
+    self.end_with?(".")
   end
-
+  
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[.?!]/).reject {|sentence| sentence.empty?}.size
+    binding.pry
+    #using a regexp, anything in the slash marks with the array of sentence endings is the pattern they are looking for so that they can split them
   end
 end
+
